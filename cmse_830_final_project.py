ce752a34e70350457a968b94d880d4ddea3bbffd
apatiras <apatiras@msu.edu>
2023-12-05 20:32:48 -0500

    requirements

--- cmse_830_final_project.py
+++ cmse_830_final_project.py
@@ -14,16 +14,12 @@ import numpy as np
 import plotly.figure_factory as ff
 import altair
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+
 from xgboost import XGBRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
 import folium
 import warnings
 
